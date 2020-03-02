b1bceef1dd1c5014bd4fcf9b7797b76d4e89bc6d
Jeremy Perry <jeremy.m.perry@gmail.com>
2020-03-02 15:32:46 -0500

    added user set up

--- config/routes.rb
+++ config/routes.rb
@@ -2,7 +2,10 @@ Rails.application.routes.draw do
   get '/login' => 'sessions#new'
   post '/login' => 'sessions#create'
   get '/logout' => 'sessions#destroy'
-  
-  resources :users
+
+  get '/signup' => 'users#new'
+  post '/users' => 'users#create'
+
+ #resources :users
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
 end
