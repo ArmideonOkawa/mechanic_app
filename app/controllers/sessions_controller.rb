81992635b3c5132a601288d62fccfa44fc7e3621
Jeremy Perry <jeremy.m.perry@gmail.com>
2020-03-02 15:26:58 -0500

    added login/logout

---
+++ app/controllers/sessions_controller.rb
@@ -0,0 +1,22 @@
+class SessionsController < ApplicationController
+    def new
+    end
+  
+    def create
+      @user = User.find_by(name: params[:name])
+      if @user && @user.authenticate(params[:password])
+        session[:user_id] = @user.id
+        flash[:success] = "Successfully logged in!"
+        redirect_to '/users'
+      else
+        flash[:warning] = "Incorrect email or password"
+        redirect_to '/login'
+      end
+    end
+  
+    def destroy
+      session[:user_id] = nil
+      flash[:success] = "Successfully Logged Out"
+      redirect_to '/login'
+    end
+end
