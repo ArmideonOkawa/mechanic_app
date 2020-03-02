f819c7720f0b85a596068c66880b141e6e392516
Jeremy Perry <jeremy.m.perry@gmail.com>
2020-03-02 18:45:17 -0500

    added create car form

--- app/controllers/cars_controller.rb
+++ app/controllers/cars_controller.rb
@@ -1,2 +1,21 @@
 class CarsController < ApplicationController
+    
+    def new
+        @car = Car.new
+    end
+
+    def show
+        @car = Car.find(params[:id])
+    end
+
+    def create
+        @car = Car.create(car_params)
+
+        redirect_to user_path
+    end
+
+    private
+    def car_params
+        params.require(:car).permit(:user_id, :make, :model, :color)
+    end
 end
