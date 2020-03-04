bcfcab0a0b2d39801397940748209f541e6f6a91
Jeremy Perry <jeremy.m.perry@gmail.com>
2020-03-04 12:38:54 -0500

    Added constant seed data

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,16 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2020_03_03_222913) do
+ActiveRecord::Schema.define(version: 2020_03_04_150457) do
+
+  create_table "appointments", force: :cascade do |t|
+    t.integer "problem_id", null: false
+    t.integer "mechanic_id", null: false
+    t.datetime "created_at", precision: 6, null: false
+    t.datetime "updated_at", precision: 6, null: false
+    t.index ["mechanic_id"], name: "index_appointments_on_mechanic_id"
+    t.index ["problem_id"], name: "index_appointments_on_problem_id"
+  end
 
   create_table "cars", force: :cascade do |t|
     t.string "make"
@@ -20,6 +29,13 @@ ActiveRecord::Schema.define(version: 2020_03_03_222913) do
     t.datetime "created_at", precision: 6, null: false
     t.datetime "updated_at", precision: 6, null: false
     t.index ["user_id"], name: "index_cars_on_user_id"
+  end
+
+  create_table "mechanics", force: :cascade do |t|
+    t.string "name"
+    t.string "specialty"
+    t.datetime "created_at", precision: 6, null: false
+    t.datetime "updated_at", precision: 6, null: false
   end
 
   create_table "problems", force: :cascade do |t|
@@ -37,6 +53,8 @@ ActiveRecord::Schema.define(version: 2020_03_03_222913) do
     t.datetime "updated_at", precision: 6, null: false
   end
 
+  add_foreign_key "appointments", "mechanics"
+  add_foreign_key "appointments", "problems"
   add_foreign_key "cars", "users"
   add_foreign_key "problems", "cars"
 end
